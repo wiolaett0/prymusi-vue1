5ec56326dae757a803a663feee4391238ae1e4ce
wiolaett0 <misswiol@gmail.com>
2020-11-13 21:12:18 +0100

    Add form

--- prymus-vue/src/components/Participants.vue
+++ prymus-vue/src/components/Participants.vue
@@ -6,17 +6,20 @@
 		<div class="participant-list">
 		<h1>Participants</h1>
 		<div v-if="participants.length> 0">
-			<div v-for="participant in participants" v-bind:key="participant">
+			<div v-for="participant in participants" v-bind:key="participamt">
 			{{participant.name}}  {{participant.surname}} 
-			<button v-on:click="onRemove(participant)" type="button" class="btn btn-danger">Remove</button>
+			<button v-on:click="onRemove(participant)" type="button" class="btn btn danger">Remove</button>
 			</div>
 			</div>
-		<div v-else class="alert alert-info" role="alert"> Ooops. Nothing here ;C</div>
+		<div v-else class="alert alert-light" role="alert> Ooops. Nothing here ;C</div>
 		</div>
-		
-	
+		<h1>List</h1>
+		<div v-if="participants.length > 0">
+			<div v-for="participant in participants" :key="participant.name">{{ participant.name}} {{ participant.lastName}} </div>
+		</div>
+		<div v-else class="alert alert-dark" role="alert">No participants yet!</div>
+		<form>
 				<h1>Add Participant</h1>
-					
 				<div class="form-group">
 			<label for="participant-name">Name</label>
 		<input class="form-control" id="participant-name" placeholder="Enter name" v-model="newParticipantName">
